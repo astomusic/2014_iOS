862d1687e35164784d33f72164a5a4029a349f22
astomusic <astomusic@naver.com>
2014-07-10 15:09:53 +0900

    Initial Commit

---
+++ WEEK1/2014_iOS_WEEK1/2014_iOS_WEEK1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  2014_iOS_WEEK1
+//
+//  Created by astomusic on 2014. 7. 10..
+//  Copyright (c) 2014년 NEXT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
