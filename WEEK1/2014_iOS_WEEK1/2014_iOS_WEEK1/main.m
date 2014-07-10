4ea62eead2cdf6f24e0fc0464c9e468db35d18ee
astomusic <astomusic@naver.com>
2014-07-10 16:17:24 +0900

    #1 #2 구현

--- WEEK1/2014_iOS_WEEK1/2014_iOS_WEEK1/main.m
+++ WEEK1/2014_iOS_WEEK1/2014_iOS_WEEK1/main.m
@@ -7,16 +7,48 @@
 //
 
 #import <Foundation/Foundation.h>
+NSMutableArray* NXDisplayAllFilesAtPath(NSString *path);
+Boolean isExistFilename(NSString *fName, NSString *path);
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        Boolean result;
         
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        result = isExistFilename(@"test.txt", @"/Users/astomusic/Desktop");
     }
     return 0;
 }
+
+NSMutableArray* NXDisplayAllFilesAtPath(NSString *path) {
+    NSFileManager *fm;
+    NSDirectoryEnumerator *dirEnum;
+    NSMutableArray *dirArray = [[NSMutableArray alloc]init];
+    NSString *file;
+    
+    fm = [NSFileManager defaultManager];
+    
+    dirEnum= [fm enumeratorAtPath:path];
+    NSLog(@"currnet directory : %@", path);
+    while ((file = [dirEnum nextObject]) != nil) {
+        [dirArray addObject:(NSString*)file];
+        NSLog(@"%@", file);
+    }
+    return dirArray;
+}
+
+Boolean isExistFilename(NSString *fName, NSString *path) {
+    NSMutableArray *dirArray;
+    dirArray = NXDisplayAllFilesAtPath(path);
+    
+    if([dirArray containsObject:fName]){
+        NSLog(@"<%@> is included in <%@>", fName, path);
+        return true;
+    }
+    
+    NSLog(@"<%@> is not included in <%@>", fName, path);
+    return false;
+}
+
 
